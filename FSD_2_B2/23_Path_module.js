42aefe3f94d37282aaaecddc0a8b339f6d737c3d
Krisha Virani <krishavirani2007@gmail.com>
2026-03-16 10:46:09 +0530

    Add files via upload

--- FSD_2_B2/23_Path_module.js
+++ FSD_2_B2/23_Path_module.js
@@ -1,21 +1,21 @@
 //Path Module 
 var p=require("path")
-var path="D:/LJ/node/myhtml";
+var path="D:/LJ/node/my.html";
 var d=p.dirname(path);
 var b=p.basename(path);
 var e=p.extname(path);
 var p1=p.parse(path);
-console.log(`Directory : ${d} \n File : ${b} \n EXtention : ${e}`);
+console.log(`Directory : ${d} \n File : ${b} \n Extention : ${e}`);
 console.log(p1)
 
 // Output -------
-// Directory : D:/LJ/node 
-//  File : myhtml
-//  EXtention :
+//Directory : D:/LJ/node 
+//  File : my.html
+//  Extention : .html
 // {
 //   root: 'D:/',
 //   dir: 'D:/LJ/node',
-//   base: 'myhtml',
-//   ext: '',
-//   name: 'myhtml'
+//   base: 'my.html',
+//   ext: '.html',
+//   name: 'my'
 // }
