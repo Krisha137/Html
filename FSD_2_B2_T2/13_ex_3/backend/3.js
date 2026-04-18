afef09aecf16e2f60a8f46609ffe5c53285bf263
Krisha Virani <krishavirani2007@gmail.com>
2026-04-18 11:51:07 +0530

    Update 3.js

--- FSD_2_B2_T2/13_ex_3/backend/3.js
+++ FSD_2_B2_T2/13_ex_3/backend/3.js
@@ -1,7 +1,23 @@
 var express = require("express");
 var app = express();
 
-var path=require("path");
-var sp=path.join(__dirname,"../frontend")
-app.use(express.static(sp,{index:"3.html"}));
+var path = require("path");
+var sp = path.join(__dirname, "../frontend");
+app.use(express.static(sp, { index: "3.html" }));
 app.listen(5000);
+
+// Other option but it will only for 3.html
+// app.get("/",(req,res)=>
+// {
+//     res.sendFile(sp+"/3.html")
+// })
+
+// like this -------
+
+// var path = require("path");
+// var sp = path.join(__dirname, "../frontend");
+// app.use(express.static(sp));
+// app.get("/", (req, res) => {
+//   res.sendFile(sp + "/3.html");
+// });
+// app.listen(5000);
